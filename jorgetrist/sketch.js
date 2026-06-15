403f1e1d95a7ab56a94cf165d30abb6db9f00e52
Barbancho1 <abarbanc@iespompeufabra.cat>
2026-06-15 08:30:58 +0200

    Create sketch.js

---
+++ jorgetrist/sketch.js
@@ -0,0 +1,204 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+ beginShape();
+  fill(153, 91, 68);
+curveVertex(324.5, 81.609375);//Cos
+curveVertex(301.5, 96.609375);
+curveVertex(239.5, 100.609375);
+curveVertex(189.5, 122.609375);
+curveVertex(169.5, 166.609375);
+curveVertex(172.5, 216.609375);
+curveVertex(159.5, 220.609375);
+curveVertex(155.5, 247.609375);
+curveVertex(178.5, 271.609375);
+curveVertex(196.5, 266.609375);
+curveVertex(243.5, 287.609375);
+curveVertex(242.5, 309.609375);
+curveVertex(200.5, 368.609375);
+curveVertex(160.5, 455.609375);
+curveVertex(172.5, 533.609375);
+curveVertex(225.5, 593.609375);
+curveVertex(235.5, 600.609375);
+curveVertex(239.5, 664.609375);
+curveVertex(231.5, 697.609375);
+curveVertex(193.5, 722.609375);
+curveVertex(185.5, 735.609375);
+curveVertex(198.5, 744.609375);
+curveVertex(201.5, 728.609375);
+curveVertex(222.5, 715.609375);
+curveVertex(203.5, 742.609375);
+curveVertex(216.5, 744.609375);
+curveVertex(222.5, 726.609375);
+curveVertex(237.5, 719.609375);
+curveVertex(221.5, 744.609375);
+curveVertex(241.5, 745.609375);
+curveVertex(261.5, 718.609375);
+curveVertex(246.5, 744.609375);
+curveVertex(286.5, 729.609375);
+curveVertex(312.5, 734.609375);
+curveVertex(322.5, 726.609375);
+curveVertex(304.5, 708.609375);
+curveVertex(301.5, 656.609375);
+curveVertex(314.5, 607.609375);
+curveVertex(321.5, 604.609375);
+curveVertex(332.5, 649.609375);
+curveVertex(317.5, 702.609375);
+curveVertex(319.5, 716.609375);
+curveVertex(352.5, 731.609375);
+curveVertex(380.5, 731.609375);
+curveVertex(374.5, 719.609375);
+curveVertex(344.5, 712.609375);
+curveVertex(402.5, 724.609375);
+curveVertex(424.5, 720.609375);
+curveVertex(386.5, 705.609375);
+curveVertex(436.5, 727.609375);
+curveVertex(435.5, 713.609375);
+curveVertex(416.5, 704.609375);
+curveVertex(450.5, 721.609375);
+curveVertex(447.5, 705.609375);
+curveVertex(388.5, 690.609375);
+curveVertex(400.5, 547.609375);
+curveVertex(444.5, 476.609375);
+curveVertex(426.5, 399.609375);
+curveVertex(387.5, 339.609375);
+curveVertex(342.5, 295.609375);
+curveVertex(328.5, 276.609375);
+curveVertex(343.5, 258.609375);
+curveVertex(374.5, 230.609375);
+curveVertex(375.5, 191.609375);
+curveVertex(354.5, 169.609375);
+curveVertex(339.5, 118.609375);
+curveVertex(331.5, 110.609375);
+curveVertex(342.5, 108.609375);
+curveVertex(330.5, 101.609375);
+curveVertex(342.5, 87.609375);
+curveVertex(326.5, 94.609375);
+curveVertex(323.5, 79.609375);
+  endShape(CLOSE);
+      fill(246, 205, 164);
+  fill(244, 49, 39)
+ beginShape();//globus vermell desinflat
+curveVertex(496.5, 688.609375);
+curveVertex(501.5, 685.609375);
+curveVertex(510.5, 680.609375);
+curveVertex(524.5, 680.609375);
+curveVertex(538.5, 689.609375);
+curveVertex(542.5, 698.609375);
+curveVertex(526.5, 705.609375);
+curveVertex(506.5, 704.609375);
+curveVertex(485.5, 701.609375);
+curveVertex(474.5, 710.609375);
+curveVertex(472.5, 694.609375);
+curveVertex(485.5, 693.609375);
+endShape(CLOSE);
+  fill(0, 160, 226)
+ beginShape();//globus blau caient
+curveVertex(479.5, 567.609375);
+curveVertex(480.5, 552.609375);
+curveVertex(468.5, 562.609375);
+curveVertex(463.5, 568.609375);
+curveVertex(478.5, 582.609375);
+curveVertex(487.5, 574.609375);
+curveVertex(503.5, 573.609375);
+curveVertex(515.5, 564.609375);
+curveVertex(526.5, 565.609375);
+curveVertex(526.5, 575.609375);
+curveVertex(511.5, 586.609375);
+curveVertex(491.5, 591.609375);
+curveVertex(479.5, 584.609375);
+endShape(CLOSE);
+  fill(247, 245, 0)
+  beginShape();//Globus blau desinflat
+curveVertex(640.5, 704.609375);
+curveVertex(654.5, 708.609375);
+curveVertex(670.5, 704.609375);
+curveVertex(676.5, 714.609375);
+curveVertex(661.5, 727.609375);
+curveVertex(640.5, 722.609375);
+curveVertex(635.5, 715.609375);
+curveVertex(624.5, 715.609375);
+curveVertex(626.5, 704.609375);
+curveVertex(634.5, 695.609375);
+curveVertex(634.5, 703.609375);
+endShape(CLOSE);
+  fill(255, 255, 255)
+  beginShape();//Braç dret
+curveVertex(230.5, 503.609375);
+curveVertex(220.5, 479.609375);
+curveVertex(258.5, 371.609375);
+curveVertex(240.5, 435.609375);
+curveVertex(232.5, 509.609375);
+curveVertex(232.5, 595.609375);
+endShape();
+  fill(255,255,255);
+ beginShape();//Braç esquerre
+vertex(397.5, 465.609375);
+vertex(378.5, 388.609375);
+vertex(350.5, 335.609375);
+endShape();
+  fill(246, 205, 164);
+beginShape(); //Cara
+curveVertex(337.5, 266.609375);
+curveVertex(288.5, 281.609375);
+curveVertex(239.5, 275.609375);
+curveVertex(201.5, 251.609375);
+curveVertex(204.5, 207.609375);
+curveVertex(187.5, 161.609375);
+curveVertex(219.5, 116.609375);
+curveVertex(267.5, 119.609375);
+curveVertex(312.5, 107.609375);
+curveVertex(348.5, 136.609375);
+curveVertex(355.5, 172.609375);
+curveVertex(379.5, 203.609375);
+curveVertex(366.5, 243.609375);
+curveVertex(338.5, 263.609375);
+endShape(CLOSE);
+   fill(246, 205, 164);
+  beginShape();//boca triste
+curveVertex(227.5, 246.609375);
+curveVertex(238.5, 237.609375);
+curveVertex(259.5, 226.609375);
+curveVertex(276.5, 221.609375);
+curveVertex(297.5, 218.609375);
+curveVertex(314.5, 222.609375);
+curveVertex(333.5, 228.609375);
+curveVertex(344.5, 234.609375);
+endShape();
+      fill(246, 205, 164);
+  beginShape();//Nas
+curveVertex(268.5, 176.609375);
+curveVertex(287.5, 164.609375);
+curveVertex(306.5, 171.609375);
+endShape(CLOSE);
+  //ulls
+  fill(255, 255, 255);
+  ellipse (238,174,27,25) //ull dret
+  fill(255, 255, 255);
+    ellipse (322,161,27,25) //pupil·la dreta
+  
+  
+  
+  fill(255, 255, 255);
+  ellipse (238,178,17,15)//ull esquerre
+
+    fill(35, 22, 6)
+  ellipse (322,165,17,15)//pupil·la esquerre
+  
+  
+  
+   fill(0, 160, 226)
+  let x=sin(frameCount*0.02)*18;
+  beginShape();//Llagrima
+curveVertex(322.5, 177.609375+x);
+curveVertex(319.5, 181.609375+x);
+curveVertex(318.5, 188.609375+x);
+curveVertex(322.5, 192.609375+x);
+curveVertex(326.5, 193.609375+x);
+curveVertex(331.5, 189.609375+x);
+curveVertex(330.5, 183.609375+x);
+curveVertex(326.5, 178.609375+x);
+endShape(CLOSE);
+}
